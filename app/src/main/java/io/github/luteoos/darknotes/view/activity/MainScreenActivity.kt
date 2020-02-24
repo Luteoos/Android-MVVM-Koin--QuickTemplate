7b8cac7da80944e8369049113bf9c5fd207b7028
Luteoos <lmati97@gmail.com>
2020-02-24 01:30:57 +0100

    added header and handling motionLayout

--- app/src/main/java/io/github/luteoos/darknotes/view/activity/MainScreenActivity.kt
+++ app/src/main/java/io/github/luteoos/darknotes/view/activity/MainScreenActivity.kt
@@ -2,12 +2,16 @@ package io.github.luteoos.darknotes.view.activity
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import io.github.luteoos.darknotes.R
 import io.github.luteoos.darknotes.utils.Session
+import io.github.luteoos.darknotes.view.fragment.MainScreenFragment
 import io.github.luteoos.darknotes.viewmodel.MainScreenViewModel
 import io.github.luteoos.mvvmbaselib.BaseActivityMVVM
 import io.github.luteoos.mvvmbaselib.Event
 import kotlinx.android.synthetic.main.activity_main_screen.*
+import kotlinx.android.synthetic.main.header_main_screen.*
+import java.lang.Exception
 
 class MainScreenActivity : BaseActivityMVVM<MainScreenViewModel>() {
 
@@ -17,9 +21,8 @@ class MainScreenActivity : BaseActivityMVVM<MainScreenViewModel>() {
         super.onCreate(savedInstanceState)
         viewModel = getViewModel(this)
         connectToVMMessage()
-        textView.setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
+        setBindings()
+        switchFragment(MainScreenFragment())
     }
 
     override fun onResume() {
@@ -27,8 +30,20 @@ class MainScreenActivity : BaseActivityMVVM<MainScreenViewModel>() {
         setUsername()
     }
 
+    private fun setBindings(){
+        btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+
+    private fun switchFragment(piece: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, piece)
+            .commitAllowingStateLoss()
+    }
+
     private fun setUsername(){
-        textView.text = getString(R.string.welcome_message, Session.username)
+        tvWelcome.text = getString(R.string.welcome_message, Session.username)
     }
 
     override fun onVMMessage(msg: Event<Int>) {
---
+++ app/src/main/java/io/github/luteoos/darknotes/view/fragment/MainScreenFragment.kt
@@ -0,0 +1,15 @@
+package io.github.luteoos.darknotes.view.fragment
+
+import android.os.Bundle
+import android.view.View
+import io.github.luteoos.darknotes.R
+import io.github.luteoos.darknotes.baseAbstract.BaseFragmentWithoutViewModel
+
+class MainScreenFragment : BaseFragmentWithoutViewModel() {
+
+    override fun getLayoutID() = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
