0f9716f982d47f8ff7d7419b8591a96c091d5c05
Luteoos <lmati97@gmail.com>
2020-02-24 15:13:07 +0100

    clean commit

--- app/src/main/java/io/github/luteoos/darknotes/baseAbstract/BaseFragment.kt
+++
@@ -1,7 +0,0 @@
-package io.github.luteoos.darknotes.baseAbstract
-
-import io.github.luteoos.mvvmbaselib.BaseFragmentMVVM
-import io.github.luteoos.mvvmbaselib.BaseViewModel
-
-abstract class BaseFragment<T: BaseViewModel> : BaseFragmentMVVM<T>() {
-}
--- app/src/main/java/io/github/luteoos/darknotes/utils/Parameters.kt
+++
@@ -1,6 +0,0 @@
-package io.github.luteoos.darknotes.utils
-
-object Parameters {
-    const val REST_DATE = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-
-}
